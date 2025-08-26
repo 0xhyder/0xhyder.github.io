9352968d75d8aede1deadadff58aa6e7a5c1cbad
dhinesh2022h <dhinesh2022h@outlook.com>
2025-08-26 16:00:56 +0530

    url

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://0xhyder.github.io",
+	site: "https://0xhyder.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
